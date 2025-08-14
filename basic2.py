02b0ce500ecdc6af9f8c6683fcd4ec370cca6a74
Sibghatullah-pk <sibghatullah1a2a3a@gmail.com>
2025-08-14 19:59:03 +0500

    basssic

--- basic2.py
+++ basic2.py
@@ -18,3 +18,12 @@ print(c)
 print(type(a))
 print(type(b))
 print(type(c))
+
+
+import requests
+
+#the required first parameter of the 'get' method is the 'url':
+x = requests.get('https://w3schools.com/python/demopage.htm')
+
+#print the response text (the content of the requested file):
+print(x.text)
